d34d35de172d9f1fb961b180b5d76fbffdc1a104
Knutsen, Daniel M 987600011 <daniel.m.knutsen@nasa.gov>
2024-07-23 21:59:57 +0000

    Fix #100, Check CFE_SB_NO_MESSAGE return code
    Fix #100, check cfe_sb_no_message return code from CFE_SB_ReceiveBuffer

--- fsw/src/mm_app.c
+++ fsw/src/mm_app.c
@@ -91,7 +91,7 @@ void MM_AppMain(void) {
     if ((Status == CFE_SUCCESS) && (BufPtr != NULL)) {
       /* Process Software Bus message */
       MM_TaskPipe(BufPtr);
-    } else if (Status == CFE_SB_TIME_OUT) {
+    } else if (Status == CFE_SB_TIME_OUT || Status == CFE_SB_NO_MESSAGE) {
       /* No action, but also no error */
     } else {
       /*
